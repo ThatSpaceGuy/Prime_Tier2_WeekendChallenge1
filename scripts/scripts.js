b03b20515687ee3e444a993cd5fcb7a94a34979c
Luis H. Matos <ReachLuis@Gmail.com>
2016-08-25 23:22:03 -0500

    Clear inputs after clicking Submit - ver 0.3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
 console.log('scripts.js sourced!');
+
+var addEmployee = function() {
+  console.log('in addEmployee');
+  // store information from the inputs - ver 0.4
+  // clear inputs - ver 0.3
+  document.getElementById('firstNameIn').value = '';
+  document.getElementById('lastNameIn').value = '';
+  document.getElementById('idNumIn').value = '';
+  document.getElementById('jobTitleIn').value = '';
+  document.getElementById('salaryIn').value = '';
+  // append information to DOM - ver 0.5
+  // display total salary - ver 1.0
+}; // end addEmployee
